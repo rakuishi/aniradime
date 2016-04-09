0fab720c14dea9453c656e249f35696002f8832d
rakuishi <rakuishi@gmail.com>
2016-04-09 18:11:23 +0900

    Add whenever to gem

--- Gemfile
+++ Gemfile
@@ -41,6 +41,9 @@ gem 'figaro'
 # Parse HTML
 gem 'nokogiri'
 
+# Cron jobs
+gem 'whenever', :require => false
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
---
+++ config/schedule.rb
@@ -0,0 +1,32 @@
+# Use this file to easily define all of your cron jobs.
+#
+# It's helpful, but not entirely necessary to understand cron before proceeding.
+# http://en.wikipedia.org/wiki/Cron
+
+# CLI MEMO
+# ./bin/bundle exec wheneverize .
+# ./bin/bundle exec whenever --update-cron
+# ./bin/bundle exec whenever --update-cron --set 'environment=development'
+# ./bin/bundle exec whenever --clear-cron
+
+every 10.minute do
+  runner 'AnimateWorker.task'
+  runner 'ChnicovideoWorker.task'
+  runner 'OnsenWorker.task'
+end
+
+# Example:
+#
+# set :output, "/path/to/my/cron_log.log"
+#
+# every 2.hours do
+#   command "/usr/bin/some_great_command"
+#   runner "MyModel.some_method"
+#   rake "some:great:rake:task"
+# end
+#
+# every 4.days do
+#   runner "AnotherModel.prune_old_records"
+# end
+
+# Learn more: http://github.com/javan/whenever

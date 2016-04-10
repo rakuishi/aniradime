926f64d8d0764432d404dc9e21215a664c998965
rakuishi <rakuishi@gmail.com>
2016-04-10 15:06:03 +0900

    一週間分のラジオを表示する

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,5 +1,15 @@
 class MainController < ApplicationController
+
+  # 一週間分のラジオを表示する
   def index
-    @radios = Radio.includes(:radio_station).references(:radio_stations).order(published_at: :desc).limit(24)
+    @contents = {}
+    for i in 0..6
+      datetime = DateTime.now - i.day
+      radios = Radio.includes(:radio_station).references(:radio_stations).order(published_at: :desc)
+        .where('published_at BETWEEN ? AND ?', datetime.beginning_of_day, datetime.end_of_day).all
+      next if radios.length == 0
+
+      @contents[datetime.strftime('%Y/%m/%d')] = radios
+    end
   end
 end

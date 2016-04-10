6a4590d35dd72ebc30042b071c5fe7a8c328dd9c
rakuishi <rakuishi@gmail.com>
2016-04-10 10:54:47 +0900

    ルートページのデザインを作成

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,5 +1,5 @@
 class MainController < ApplicationController
   def index
-    @radios = Radio.includes(:radio_station).references(:radio_stations).order(published_at: :desc).limit(12)
+    @radios = Radio.includes(:radio_station).references(:radio_stations).order(published_at: :desc).limit(24)
   end
 end

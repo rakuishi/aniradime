8e921fa31cd485bad5e63e81c48651721766e936
rakuishi <rakuishi@gmail.com>
2016-04-10 12:34:20 +0900

    画像はサーバーに保存して使用する

--- app/models/radio.rb
+++ app/models/radio.rb
@@ -1,9 +1,14 @@
+require 'digest/md5'
+require 'open-uri'
+require 'fileutils'
+
 class Radio < ActiveRecord::Base
   belongs_to :radio_station
 
-  # TODO: 画像はサーバーに保存して使用する
   def self.create_or_update_with(name, description, url, image_url, published_at, radio_station)
+    image_url = download_image(image_url)
     published_at = Time.parse(published_at).to_datetime
+    p image_url
 
     radio = Radio.find_or_initialize_by(name: name, published_at: published_at)
     radio.name = name
@@ -13,5 +18,22 @@ class Radio < ActiveRecord::Base
     radio.published_at = published_at
     radio.radio_station = radio_station
     radio.save
+  end
+
+  def self.download_image(image_url)
+    return image_url unless image_url.present?
+
+    md5 = Digest::MD5.new.update(image_url).to_s
+    public_dir = "#{Rails.root}/public"
+    path = "/uploads/#{md5.slice(0, 2)}/#{md5}"
+    return path if File.exist?(public_dir + path)
+
+    begin
+      FileUtils.mkdir_p(File.dirname(public_dir + path))
+      IO.copy_stream(open(image_url), public_dir + path)
+      path
+    rescue Exception => e
+      image_url
+    end
   end
 end

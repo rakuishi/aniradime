000e2eff8eed3146791c4a2fd4cf93fcf8a91f9c
rakuishi <rakuishi@gmail.com>
2016-04-11 21:31:03 +0900

    Add unicorn.rb

---
+++ config/unicorn.rb
@@ -0,0 +1,27 @@
+APP_PATH = "/home/apps/aniradi.me"
+
+worker_processes 4
+working_directory APP_PATH
+listen "/var/run/unicorn/unicorn.socket"
+pid APP_PATH + "/tmp/pids/unicorn.pid"
+stderr_path APP_PATH + "/log/unicorn.log"
+stdout_path APP_PATH + "/log/unicorn.log"
+
+preload_app true
+
+before_fork do |server, worker|
+  ActiveRecord::Base.connection.disconnect!
+
+  old_pid = "#{server.config[:pid]}.oldbin"
+  if old_pid != server.pid
+    begin
+      sig = (worker.nr + 1) >= server.worker_processes ? :QUIT : :TTOU
+      Process.kill(sig, File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH
+    end
+  end
+end
+
+after_fork do |server, worker|
+  ActiveRecord::Base.establish_connection
+end

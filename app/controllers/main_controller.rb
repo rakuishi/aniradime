bcdbc94889c3987ab6b9d58624d7ad42b7a4ed4c
rakuishi <rakuishi@gmail.com>
2016-04-15 23:20:24 +0900

    Add load more button #2

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,8 +2,12 @@ class MainController < ApplicationController
 
   # 一週間分のラジオを表示する
   def index
+    num = params[:num] || 1
+    num = num.to_i - 1
+    @next = num + 2
+
     @contents = {}
-    for i in 0..6
+    for i in (num * 7)..(num * 7 + 6)
       datetime = DateTime.now - i.day
       radios = Radio.includes(:radio_station).references(:radio_stations).order(published_at: :desc)
         .where('published_at BETWEEN ? AND ?', datetime.beginning_of_day, datetime.end_of_day).all

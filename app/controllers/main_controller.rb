f9cc84dfa597c30a22d62528dd200f6f901a2a55
rakuishi <rakuishi@gmail.com>
2016-04-16 08:59:20 +0900

    曜日を表示する

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -7,13 +7,15 @@ class MainController < ApplicationController
     @next = num + 2
 
     @contents = {}
+    weeks = %w(Sun Mon Tue Wed Thu Fri Sat)
     for i in (num * 7)..(num * 7 + 6)
       datetime = DateTime.now - i.day
       radios = Radio.includes(:radio_station).references(:radio_stations).order(published_at: :desc)
         .where('published_at BETWEEN ? AND ?', datetime.beginning_of_day, datetime.end_of_day).all
       next if radios.length == 0
 
-      @contents[datetime.strftime('%Y/%m/%d')] = radios
+      key = datetime.strftime('%Y/%m/%d') + ' ' + weeks[datetime.strftime('%w').to_i]
+      @contents[key] = radios
     end
   end
 end

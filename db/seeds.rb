c3d8daf02201e90d63ce11c2558910db5b88b97f
rakuishi <rakuishi@gmail.com>
2016-04-16 10:56:50 +0900

    Add 超！アニメディア

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 def setupRadioStations
-  RadioStation.first_or_create([
+  stations = [
     {
       id: 1,
       name: 'シーサイド',
@@ -35,8 +35,24 @@ def setupRadioStations
       url: 'http://www.animate.tv/radio/',
       parse_url: 'http://www.animate.tv/radio/',
       parse_url_type: 'animate'
+    },
+    {
+      id: 5,
+      name: '超！アニメディア',
+      url: 'http://ch.nicovideo.jp/animedia',
+      parse_url: 'http://ch.nicovideo.jp/animedia/video?rss=2.0',
+      parse_url_type: 'chnicovideo',
     }
-  ])
+  ]
+
+  stations.each do |station|
+    RadioStation.find_or_initialize_by(id: station[:id]).update_attributes(
+      name: station[:name],
+      url: station[:url],
+      parse_url: station[:parse_url],
+      parse_url_type: station[:parse_url_type],
+    )
+  end
 end
 
 setupRadioStations()
